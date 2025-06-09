2548240656a4cbc25c867d8c46284eda89ebdb97
julesbruins <jf-bruins@hotmail.com>
2025-06-09 15:07:29 +0200

    data + server till common data #1
    i've added a navigation for the 6 types of employees. I added the common data for each of them.

--- server.js
+++ server.js
@@ -1,1 +1,40 @@
-console.log('Hier komt je server voor Sprint 12.')
+import express from 'express'
+import { Liquid } from 'liquidjs';
+ 
+const app = express()
+ 
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
+ 
+
+const engine = new Liquid()
+app.engine('liquid', engine.express())
+
+app.set('views', './views')
+
+
+
+app.get('/', function (request, response) {
+    response.render('index.liquid')
+})
+
+app.get('/:role', async function (request, response) {
+    const role = request.params.role
+    const dynamicRole = await fetch(`https://fdnd-fresk-api.netlify.app/get-content-by-role?userRole=${role}`)
+    const dynamicRoleJSON = await dynamicRole.json()
+
+    response.render('roles.liquid', {
+      roles: dynamicRoleJSON.commonData,
+      roleSpecificData: dynamicRoleJSON.roleSpecificData
+    })
+})
+
+
+  
+
+
+app.set('port', process.env.PORT || 8000)
+ 
+app.listen(app.get('port'), function () {
+  console.log(`Project draait via http://localhost:${app.get('port')}/\n\nSucces deze sprint. En maak mooie dingen! 🙂`)
+})
